d5043334074f97608596a77d9cf4efd007200b86
Anders Eggers-Krag <aek@innonova.ch>
2024-12-02 10:07:20 +0100

    v2.0.5

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,2 @@
-
-	export const version = '2.0.3'
-	export const releaseDate = new Date('2024-11-12T13:34:32.542Z')
+export const version = '2.0.5'
+export const releaseDate = new Date('2024-12-02T10:06:32.542Z')
