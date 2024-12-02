842e83a25f210586791f83841ef31932957924c5
Anders Eggers-Krag <aek@innonova.ch>
2024-12-02 10:22:54 +0100

    v2.0.6

--- build-all-linux.sh
+++ build-all-linux.sh
@@ -3,6 +3,7 @@ mkdir certs
 cp ../../../certs/* ./certs/
 cp ../../../env/mimiri-client.env ./.env
 npm run enable-prop-icons
+npm run set-version
 npm run build
 npm run make-bundle -- 2024101797F6C918
 npm run deploy-web
---
+++ scripts/set-version.js
@@ -0,0 +1,13 @@
+import { readFileSync, writeFileSync } from 'fs';
+import Path from 'path';
+
+
+const pack = JSON.parse(readFileSync('./package.json'))
+const versionTsPath = Path.join(vuePath, 'src/version.ts');
+
+const releaseDate = new Date().toISOString();
+
+writeFileSync(versionTsPath, `
+	export const version = '${pack.version}'
+	export const releaseDate = new Date('${releaseDate}')
+`)
