3bba7c67c2998a235b3367b8086ad329c7124520
Anders Eggers-Krag <aek@innonova.ch>
2024-12-17 14:20:12 +0100

    update icons

--- build-all-linux.sh
+++ build-all-linux.sh
@@ -2,7 +2,6 @@ npm install
 mkdir certs
 cp ../../../certs/* ./certs/
 cp ../../../env/mimiri-client.env ./.env
-npm run enable-prop-icons
 npm run set-version
 npm run build
 npm run make-bundle -- 2024101797F6C918
