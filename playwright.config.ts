565dbabc5fde8c0e5452f2c29341579ba5ff560e
Daniel Moeller <djm@innonova.ch>
2025-01-09 10:27:58 +0100

    prettier fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,35 +6,35 @@ const useWebServer = process.env.NO_WEB_SERVER === undefined ? true : undefined
 const isCi = !!process.env.CI
 
 const config: PlaywrightTestConfig = {
-  use: {
-    headless: false,
-    viewport: { width: 1280, height: 720 },
-    ignoreHTTPSErrors: true,
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure',
-    baseURL: testServerUrl,
-    screenshot: 'only-on-failure',
-  },
-  testMatch: 'playwright/*.spec.ts',
-  retries: 0,
-  fullyParallel: true,
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'], channel: 'chromium' },
-    },
-  ],
-  webServer: useWebServer && {
-    command: `npm run dev -- --port ${port}`,
-    url: testServerUrl,
-    timeout: 30_000,
-  },
-  // fail fast on ci to keep ci-minutes low
-  maxFailures: isCi ? 1 : undefined,
-  expect: {
-    timeout: 30_000,
-  },
-  timeout: 30_000,
+	use: {
+		headless: false,
+		viewport: { width: 1280, height: 720 },
+		ignoreHTTPSErrors: true,
+		trace: 'retain-on-failure',
+		video: 'retain-on-failure',
+		baseURL: testServerUrl,
+		screenshot: 'only-on-failure',
+	},
+	testMatch: 'playwright/*.spec.ts',
+	retries: 0,
+	fullyParallel: true,
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'], channel: 'chromium' },
+		},
+	],
+	webServer: useWebServer && {
+		command: `npm run dev -- --port ${port}`,
+		url: testServerUrl,
+		timeout: 30_000,
+	},
+	// fail fast on ci to keep ci-minutes low
+	maxFailures: isCi ? 1 : undefined,
+	expect: {
+		timeout: 30_000,
+	},
+	timeout: 30_000,
 }
 
 export default config
--- playwright/e2e.spec.ts
+++ playwright/e2e.spec.ts
@@ -4,25 +4,25 @@ import { generateRandomString } from './utils'
 const randomId = generateRandomString(8)
 
 test.describe('Unauthorized', () => {
-    test(`should create account`, async ({ page }) => {
-      await page.goto('/')
-      await page.getByTestId('create-account-link').click()
+	test(`should create account`, async ({ page }) => {
+		await page.goto('/')
+		await page.getByTestId('create-account-link').click()
 
-      const usernameInput = page.getByTestId('username-input')
-      const passwordInput = page.getByTestId('password-input')
-      const repeatInput = page.getByTestId('repeat-input')
-      const privacyCheckbox = page.getByTestId('privacy-checkbox')
-      const noRecoverCheckbox = page.getByTestId('no-recover-checkbox')
-      const firstNode = page.getByTestId('tree-node').first()
-  
-      await usernameInput.fill('test_automation_' + randomId)
-      await passwordInput.fill('password_' + randomId)
-      await repeatInput.fill('password_' + randomId)
-      await privacyCheckbox.check()
-      await noRecoverCheckbox.check()
+		const usernameInput = page.getByTestId('username-input')
+		const passwordInput = page.getByTestId('password-input')
+		const repeatInput = page.getByTestId('repeat-input')
+		const privacyCheckbox = page.getByTestId('privacy-checkbox')
+		const noRecoverCheckbox = page.getByTestId('no-recover-checkbox')
+		const firstNode = page.getByTestId('tree-node').first()
 
-      await page.getByTestId('create-button').click()      
+		await usernameInput.fill('test_automation_' + randomId)
+		await passwordInput.fill('password_' + randomId)
+		await repeatInput.fill('password_' + randomId)
+		await privacyCheckbox.check()
+		await noRecoverCheckbox.check()
 
-      await expect(firstNode).toHaveText('Getting Started')
-    })
-  })
+		await page.getByTestId('create-button').click()
+
+		await expect(firstNode).toHaveText('Getting Started')
+	})
+})
