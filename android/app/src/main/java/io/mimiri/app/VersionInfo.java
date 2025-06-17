4ca77b3924191e118a0ca584ea18a1fda4b6578f
Anders Eggers-Krag <aek@innonova.ch>
2025-06-17 07:19:40 +0200

    ios/android release data

--- android/app/src/main/java/io/mimiri/app/VersionInfo.java
+++ android/app/src/main/java/io/mimiri/app/VersionInfo.java
@@ -1,7 +1,7 @@
 package io.mimiri.app;
 
 public class VersionInfo {
-	public static final String BaseVersion = "2.3.25";
-	public static final String HostVersion = "2.3.25";
-	public static final String ReleaseDate = "2025-05-22T11:13:46.290Z";
+	public static final String BaseVersion = "2.4.31";
+	public static final String HostVersion = "2.4.31";
+	public static final String ReleaseDate = "2025-06-16T12:47:36.213Z";
 }
--- ios/App/App/VersionInfo.swift
+++ ios/App/App/VersionInfo.swift
@@ -1,7 +1,7 @@
 import Foundation
 public class VersionInfo {
-  public static let BaseVersion: String = "2.3.25"
-  public static let HostVersion: String = "2.3.25"
-  public static let ReleaseDate: String = "2025-05-22T11:13:46.290Z"
+  public static let BaseVersion: String = "2.4.31"
+  public static let HostVersion: String = "2.4.31"
+  public static let ReleaseDate: String = "2025-06-16T12:47:36.213Z"
 }
 
