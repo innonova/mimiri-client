078fa3b9ca4785c82e28a3c20a64d0e8dd7cde09
Anders Eggers-Krag <aek@innonova.ch>
2025-06-25 20:49:45 +0200

    refactor mimiri store

--- src/services/storage/shared-state.ts
+++
@@ -1,4 +0,0 @@
-import type { Guid } from '../types/guid'
-import type { SymmetricCrypt } from '../symmetric-crypt'
-import type { Note } from '../types/note'
-import type { ClientConfig } from '../types/responses'
