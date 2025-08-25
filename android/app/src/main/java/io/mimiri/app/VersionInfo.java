e4034390e82f07a5271969c61023f72ac763387d
Anders Eggers-Krag <aek@innonova.ch>
2025-08-25 11:27:55 +0200

    update android

--- android/app/src/main/java/io/mimiri/app/VersionInfo.java
+++ android/app/src/main/java/io/mimiri/app/VersionInfo.java
@@ -1,7 +1,7 @@
 package io.mimiri.app;
 
 public class VersionInfo {
-	public static final String BaseVersion = "2.4.33";
-	public static final String HostVersion = "2.4.33";
-	public static final String ReleaseDate = "2025-06-17T07:24:06.826Z";
+	public static final String BaseVersion = "2.5.12";
+	public static final String HostVersion = "2.5.12";
+	public static final String ReleaseDate = "2025-08-25T04:52:23.557Z";
 }
