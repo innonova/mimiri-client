7819e2da4f1e9681a9790cba909ff9eb6eaef866
Daniel Moeller <djm@innonova.ch>
2026-06-01 11:28:53 +0200

    Fix global import declaration

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="vite/client" />
-declare const __DEV_VERSION__: string
+
+declare global {
+	const __DEV_VERSION__: string
+}
 
 import type { InterpolationParams } from './src/services/localization'
 
